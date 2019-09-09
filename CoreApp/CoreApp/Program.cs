a1ea255f047b7a132ff9a163125d42d39c880006
Umang <umangawasthy0111@gmail.com>
2019-09-10 01:45:51 +0530

    Add Core Project 2.1

---
+++ CoreApp/CoreApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using StandardLibrary;
+
+namespace CoreApp
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            IMathOperationService mathService = new MathOperationService(20, 10);
+            Console.WriteLine($"Add - {mathService.Add()}");
+            Console.WriteLine($"Sub - {mathService.Sub()}");
+            Console.WriteLine($"Div - {mathService.Div()}");
+            Console.WriteLine($"Mul - {mathService.Mul()}");
+            Console.ReadLine();
+        }
+    }
+}
+ 
