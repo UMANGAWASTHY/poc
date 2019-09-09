6f40be287affcf3da19c395c51364299e3c0fed6
Umang <umangawasthy0111@gmail.com>
2019-09-10 01:51:01 +0530

    Added StandardLibrary

---
+++ StandardLibrary/StandardLibrary/IMathOperationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary
+{
+    public interface IMathOperationService
+    {
+        int Add();
+        int Sub();
+        int Mul();
+        int Div();
+    }
+}
---
+++ StandardLibrary/StandardLibrary/MathOperationService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StandardLibrary
+{
+    public class MathOperationService : IMathOperationService
+    {
+        int a = 0;
+        int b = 0;
+        public MathOperationService(int first, int sec)
+        {
+            this.a = first;
+            this.b = sec;
+        }
+        public int Add()
+        {
+            return a + b;
+        }
+
+        public int Div()
+        {
+            return a / b;
+        }
+
+        public int Mul()
+        {
+            return a * b;
+        }
+
+        public int Sub()
+        {
+            return a - b;
+        }
+    }
+}
